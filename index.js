38498170e8ec3ae61322f313c2cf768975678d17
Justin Fokes <justin.fokes@irco.com>
2019-07-16 08:26:49 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+ * Pre-req: have node.js installed. https://nodejs.org/en/
+ * Navigate to this folder (cd ~/Desktop/repo-name)
+ * Run node index.js from root directory
+ */
+
+function buildTree(){
+    // implement your build tree here.
+}
+
+// eventually, we'll add a testing framework to be able to make assertions on what should be outputted from the function
+console.log(buildTree())
+
+
+function fizzBuzz(){
+    // implement fizzBuzz
+}
+
+console.log(fizzBuzz())
